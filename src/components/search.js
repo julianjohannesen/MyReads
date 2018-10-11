c497c53a44468d7b4a445f66ad3f937179fbae89
Julian Johannesen <julian.johannesen@gmail.com>
2018-10-11 16:10:40 -0400

    Added search adn list books components

---
+++ src/components/search.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+export default class Search extends Component {
+  render() {
+    return (
+      // Should this be a separate component?
+      <div className="search-books">
+
+        <div className="search-books-bar">
+          <Link
+            to=""
+            className="close-search"
+            onClick={() => this.setState({ showSearchPage: false })}
+          >
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" />
+          </div>
+        </div>
+
+        <div className="search-books-results">
+          <ol className="books-grid" ></ol>
+        </div>
+
+      </div>
+    );
+  }
+}
